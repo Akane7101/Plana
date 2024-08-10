c9bd70e10809b33258a3a21bf8876127874ea126
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-08-10 17:15:10 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -1014,65 +1014,8 @@ Typed *surrender* to surrender and admited defeat`
   const media = await getBuffer("https://media1.tenor.com/m/L4QUJbE-Zc8AAAAC/cat-cat-side-eye.gif");
           let encmedia = await A17.sendVideoAsSticker(m.chat, media, m, { packname: global.packname, author: global.author })
           await fs.unlinkSync(encmedia);	    
-   }
-
-
-    if (smallinput.includes('بلانا عزي الشباب')) {
-    const media = await getBuffer(`https://graph.org/file/3d609590885473d7080b2.jpg`)
-    if (isCreator) {
-	   let msg = generateWAMessageFromContent(m.key.remoteJid, {
-	     viewOnceMessage: {
-              message: {
-                "messageContextInfo": {
-                  "deviceListMetadata": {},
-                  "deviceListMetadataVersion": 2
-                },
-                interactiveMessage: proto.Message.InteractiveMessage.create({
-                  body: proto.Message.InteractiveMessage.Body.create({
-                    text: `ميريا`
-                  }),
-                  footer: proto.Message.InteractiveMessage.Footer.create({
-                    text: "افتحوا الشغل"
-                  }),
-                  header: proto.Message.InteractiveMessage.Header.create({
-                    ...(await prepareWAMessageMedia({ image: media }, { upload: A17.waUploadToServer })),
-
-
-                    title: "خمسة مليون",
-                    subtitle: "Browse through the available commands",
-                    hasMediaAttachment: false
-                  }),
-                  nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-                    buttons: [
-                      {
-                        "name": "quick_reply",
-                        "buttonParamsJson": `{"display_text":"get","id":"${prefix}gg119"}`
-                      }, 
-			    
-                    ]
-                  })
-                })
-              }
-		}
-          }, {});
-
-
-          if (!msg || !msg.key || !msg.key.remoteJid || !msg.key.id) {
-            const errorMessage = 'Error: Invalid message key.';
-            console.error(errorMessage);
-            return reply(errorMessage);
-          }
-
-          await A17.relayMessage(msg.key.remoteJid, msg.message, {
-            messageId: msg.key.id
-          });
-}
-    }
-
-
-	
-
-     
+   }	
+    
  
 	//  if (smallinput.includes('tt')) {
  //  const toSticker = require('wa-leal-stickers');
@@ -3466,6 +3409,60 @@ const shiroko = await axios.get(apiUrl, { params: parameters })
         const give1 = eco.give(user, cara, 5000000)
         reply(`*You got 💎5000000*`)
         break; 
+
+
+	case 'عزي':  
+	const media = await getBuffer(`https://graph.org/file/3d609590885473d7080b2.jpg`)
+    if (isCreator) {
+	   let msg = generateWAMessageFromContent(m.key.remoteJid, {
+	     viewOnceMessage: {
+              message: {
+                "messageContextInfo": {
+                  "deviceListMetadata": {},
+                  "deviceListMetadataVersion": 2
+                },
+                interactiveMessage: proto.Message.InteractiveMessage.create({
+                  body: proto.Message.InteractiveMessage.Body.create({
+                    text: `ميريا`
+                  }),
+                  footer: proto.Message.InteractiveMessage.Footer.create({
+                    text: "افتحوا الشغل"
+                  }),
+                  header: proto.Message.InteractiveMessage.Header.create({
+                    ...(await prepareWAMessageMedia({ image: media }, { upload: A17.waUploadToServer })),
+
+
+                    title: "خمسة مليون",
+                    subtitle: "Browse through the available commands",
+                    hasMediaAttachment: false
+                  }),
+                  nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+                    buttons: [
+                      {
+                        "name": "quick_reply",
+                        "buttonParamsJson": `{"display_text":"get","id":"${prefix}gg119"}`
+                      }, 
+			    
+                    ]
+                  })
+                })
+              }
+		}
+          }, {});
+
+
+          if (!msg || !msg.key || !msg.key.remoteJid || !msg.key.id) {
+            const errorMessage = 'Error: Invalid message key.';
+            console.error(errorMessage);
+            return reply(errorMessage);
+          }
+
+          await A17.relayMessage(msg.key.remoteJid, msg.message, {
+            messageId: msg.key.id
+          });
+}
+    }
+	  break;
 		    
 
 	case 'kkk': { 
