a6f018621db40bbe3b1619b42c89bc41b9a10c9b
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-08-22 01:26:45 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -84,7 +84,7 @@ if (time2 < "05:00:00") {
 
 
 
-// hhhjkjgkkhghjhkكkhkmkkkjjkاgkkhgkjghgاggk
+// hhhjkjgkkhghjhkكkhkmkkkjjkاggkkhgkjghgاggk
 const timestampe = speed();
 const latensie = speed() - timestampe
 
