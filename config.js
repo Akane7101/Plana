b7a334b69d1e0888b1c4451a578221b361c793db
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-08-25 22:09:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ global.antitags = true;
 //auto functioner
 global.autoTyping = false;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
-global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.groupevent = true;                //This is the new variable for controlling group event handling.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = true;
 
 
