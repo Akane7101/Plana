d32ced21321efad474930bf11ef1d0ab6812473e
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-09-15 01:07:35 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -2438,19 +2438,24 @@ const shiroko = await axios.get(apiUrl, { params: parameters })
         break; 
        
 
-	case 'tst':{
+	case 'tst': {
+  if (isBanChat) return reply(mess.bangc);
+  A17.sendMessage(from, { react: { text: "🫡", key: m.key } });
 
-        if (isBanChat) return reply(mess.bangc);
-         A17.sendMessage(from, { react: { text: "🫡", key: m.key } })
-          const imgbbUploader = require("imgbb-uploader");
-	let media = await A17.downloadAndSaveMediaMessage(quoted)
-         imgbbUploader("d5c5715bd26a25090da6c2ab87d5ed3a", `${media}`)
-         .then((response) => console.log(response))
-         .catch((error) => console.error(error));
-	 const urll = response.data.url
-	 reply(urll) 
-          }
-        break;  
+  const imgbbUploader = require("imgbb-uploader");
+  let media = await A17.downloadAndSaveMediaMessage(quoted);
+
+  // Define `urll` inside the `then` block
+  imgbbUploader("d5c5715bd26a25090da6c2ab87d5ed3a", media)
+    .then((response) => {
+      console.log(response); 
+      const urll = response.data.url;
+      reply(urll); 
+    })
+    .catch((error) => console.error(error));
+
+  break; 
+}
 
 
         case 'removebackground':
