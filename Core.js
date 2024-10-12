ac6d7d1f1ac512f1446c72cdf2f00cceb2f12cb4
Akane7101 <129458531+Akane7101@users.noreply.github.com>
2024-10-13 00:58:34 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -22,7 +22,7 @@ const kaitime = moment.tz('Asia/Kolkata').format('HH:mm:ss');
 const kaidate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY');
 const time2 = moment().tz('Asia/Kolkata').format('HH:mm:ss');
 const currentDate = new Date();
-const options = { weekday: 'long' }; // Specify 'long' to get the full day name
+const options = { weekday: 'long' }; // Specify 'long' to get the fukll day name
 const currentDay = new Intl.DateTimeFormat('en-US', options).format(currentDate);
 
 const speed = require('performance-now');
